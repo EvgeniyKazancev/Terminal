3b1c6bbd7d654a31dee16d97881217bd44807012
EvgeniyKazancev <ekazancev777@gmail.com>
2023-08-29 23:22:13 +0300

    RUUN

---
+++ src/main/java/com/example/Terminal/Entity/Balance.java
@@ -0,0 +1,19 @@
+package com.example.Terminal.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Balance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double amount;
+}
---
+++ src/main/java/com/example/Terminal/Service/BalanceService.java
@@ -0,0 +1,13 @@
+package com.example.Terminal.Service;
+
+import com.example.Terminal.Entity.Balance;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BalanceService {
+
+    public Balance getBalance(int id){
+      return
+  }
+
+}
