ef6af57189c65be1931997d20964e1d047f44afb
EvgeniyKazancev <ekazancev777@gmail.com>
2023-09-13 15:51:00 +0300

    RUUN

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into  Balance (balance) values (1234),(63642),(3837)
-insert into Users (firstName,lastName) values (Ivan,Kazancev),(Yaroslav,Kazancev)
+insert into Users (first_name,last_name) values (Ivan,Kazancev),(Yaroslav,Kazancev),(Evgeniy,Kazancev)
+insert into  Balance (balance,user_id) values (1234,1),(63642,2),(3837,3)
