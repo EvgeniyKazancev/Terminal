e91c224d82f7865ab3a9f8dcbf16b4500a2e5f33
EvgeniyKazancev <ekazancev777@gmail.com>
2023-09-13 19:01:42 +0300

    RUUN

--- src/main/java/com/example/terminal/service/OperationService.java
+++ src/main/java/com/example/terminal/service/OperationService.java
@@ -3,28 +3,35 @@ package com.example.terminal.service;
 import com.example.terminal.entity.Operation;
 import com.example.terminal.enums.OperationType;
 import com.example.terminal.repository.OperationRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Service
 public class OperationService {
-   private final OperationRepository operationRepository;
+    private final OperationRepository operationRepository;
 
     public OperationService(OperationRepository operationRepository) {
         this.operationRepository = operationRepository;
     }
 
-    public List<Operation> getOperationList(Long userId, ZonedDateTime startDate, ZonedDateTime finishDate){
-        List<Operation> operationList = operationRepository.findOperationByDateBetween(userId,startDate,finishDate);
-        return  operationList;
+
+    public List<Operation> getOperationList(Long userId, ZonedDateTime startDate, ZonedDateTime finishDate) {
+        List<Operation> operationList = operationRepository.findOperationByDateBetween(userId, startDate, finishDate);
+        if (operationList.isEmpty()){
+            throw  new EntityNotFoundException("Данные в указанный период отсутствуют");
+        }
+        return operationList;
     }
-    public  Operation addOperation(OperationType type, Long summa , Long userId){
-         Operation operation = new Operation();
-         operation.setOperationType(type);
-         operation.setSumma(summa);
-         operation.setId(userId);
-         return operationRepository.saveAndFlush(operation);
+
+    public Operation addOperation(OperationType type, Long summa, Long userId) {
+        Operation operation = new Operation();
+        operation.setOperationType(type);
+        operation.setSumma(summa);
+        operation.setId(userId);
+        return operationRepository.saveAndFlush(operation);
     }
 }
