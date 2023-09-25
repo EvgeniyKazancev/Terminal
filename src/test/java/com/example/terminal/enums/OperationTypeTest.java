1bb206935d6b006b7a77e0898c57916d1c13b0d3
EvgeniyKazancev <ekazancev777@gmail.com>
2023-09-25 17:35:40 +0300

    RUUN

---
+++ src/test/java/com/example/terminal/enums/OperationTypeTest.java
@@ -0,0 +1,22 @@
+//package com.example.terminal.enums;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//@ExtendWith(MockitoExtension.class)
+//enum OperationTypeTest {
+//    PUT_MONEY ("Put_Money"),
+//    TAKE_MONEY ("Take_money"),
+//    TRANSFER_RECEIVING ("Transfer_money_receiving"),
+//    TRANSFER_SEND ("Transfer_money_send");
+//
+//   private String type;
+//
+//    OperationTypeTest(String type) {
+//        this.type = type;
+//    }
+//
+//
+//}
--- src/test/java/com/example/terminal/service/BalanceServiceTest.java
+++ src/test/java/com/example/terminal/service/BalanceServiceTest.java
@@ -1,24 +1,33 @@
 package com.example.terminal.service;
 
 import com.example.terminal.entity.Balance;
+import com.example.terminal.enums.OperationType;
+import com.example.terminal.enums.ResponseResult;
 import com.example.terminal.repository.BalanceRepository;
 
+import com.example.terminal.response.ResponseMessage;
+import jakarta.persistence.EntityNotFoundException;
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import net.minidev.json.annotate.JsonIgnore;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+
 import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 class BalanceServiceTest {
+
 
     @Mock
     BalanceRepository balanceRepository;
@@ -29,23 +38,14 @@ class BalanceServiceTest {
     @InjectMocks
     private BalanceService balanceService;
 
-    BalanceServiceTest(BalanceRepository balanceRepository, OperationService operationService) {
-        this.balanceRepository = balanceRepository;
-        this.operationService = operationService;
+    @BeforeEach
+    public void setUp() {
+        balanceService = new BalanceService(balanceRepository, operationService, null, null);
     }
 
-    @Test
-    public void testGetBalance() {
-        Balance actual = getTestBalance();
-        System.out.println(actual);
-        when(balanceRepository.findByUserId(anyLong())).thenReturn(Optional.of(actual));
-        Balance expected = balanceService.getBalance(1L);
-        System.out.println(expected);
-        Assertions.assertEquals(actual, expected);
-
-    }
 
     private Balance getTestBalance() {
+
         Balance balance = new Balance();
         balance.setId(1L);
         balance.setBalance(1000L);
@@ -53,5 +53,31 @@ class BalanceServiceTest {
 
     }
 
+    @Test
+    public void testGetBalance() {
+
+        Balance actual = getTestBalance();
+
+        when(balanceRepository.findByUserId(actual.getId())).thenReturn(Optional.of(actual));
+        Balance expected = balanceService.getBalance(actual.getId());
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testPutMoney() {
+
+        Long summa = 100L;
+        Balance bal = getTestBalance();
+        Long balRes = bal.getBalance();
+        when(balanceRepository.findByUserId(bal.getId())).thenReturn(Optional.of(bal));
+        ResponseMessage expected = new ResponseMessage("Операция прошла успешно.", ResponseResult.SUCCESSFUL_OPERATION.getResult());
+        ResponseMessage actual = balanceService.putMoney(bal.getId(),summa);
+        System.out.println(actual.getMessage());
+        System.out.println(expected.getMessage());;
+        Assertions.assertEquals(expected.getMessage(),actual.getMessage());
+        Assertions.assertEquals(balRes +summa,bal.getBalance());
+    }
 
 }
